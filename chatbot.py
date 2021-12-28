7a9157337e1c5873cfff7c707594d6900f138184
bart-khalid <bartyouger@gmail.com>
2021-12-28 05:12:07 +0100

    first commit

---
+++ chatbot.py
@@ -0,0 +1,129 @@
+import streamlit as st
+from PIL import Image
+import time
+from functions import Functions as fct
+
+im = Image.open("robot.png")
+# config
+st.set_page_config(
+     page_title="BARTAOUCH Khalid Chatbot App",
+     page_icon=im,
+     layout="wide",
+     menu_items={
+         'About': "# BARTAOUCH KHALID CHATBOT APP. This is a CHATBOT app!"
+     }
+ )
+
+# function 
+def sidebar():
+        with st.spinner('Wait for it...'):
+            time.sleep(0.2)
+
+            l = st.session_state.conversation
+            l.reverse()
+            for i,j in enumerate(l):
+                if i%2 != 0:
+                    tem = l[i]
+                    l[i] = l[i-1]
+                    l[i-1] = tem
+
+            for i,j in enumerate(l):
+
+                if "--- ***** ---" in j:
+                    j = j.split("--- ***** ---")
+                    st.sidebar.markdown(f'<p style="color:#B85252">{"Khalid (Bot) : "}</p>', unsafe_allow_html=True)
+                    for k in j:
+                        st.sidebar.write(k)
+                    st.sidebar.write(":heavy_minus_sign:" * 15)
+                    continue
+
+                if i%2 == 0:
+                    st.sidebar.write(int((len(l)-i)/2))
+                    st.sidebar.markdown(f'<p style="color:#B85252">{"You : "}</p>', unsafe_allow_html=True)  
+                else :
+                    st.sidebar.markdown(f'<p style="color:#B85252">{"Khalid (Bot) : "}</p>', unsafe_allow_html=True)
+                        
+                st.sidebar.write(j)
+                if i%2 != 0:
+                    st.sidebar.write(":heavy_minus_sign:" * 15)    
+
+# start
+st.markdown(f'<h1 style="color:#EA5C2B;text-align: center">{"BARTAOUCH KHALID CHATBOT"}</h1>', unsafe_allow_html=True)
+
+col1, col2 = st.columns([8, 8])
+if "conversation" not in st.session_state:
+    st.session_state.conversation = []
+
+
+with col1:
+    with st.spinner('Wait for it...'):
+        time.sleep(0.2)
+        st.markdown(f'<h3 style="color:#D8B6A4">{"This chatbot is made to let you get any information about me !"}</h3>', unsafe_allow_html=True)
+        st.subheader("Hope you'll like it 😄😄")
+        st.info("Example of questions you could type (click on any button to see the result)")
+        exmple_1 = st.button("Who are you ?")
+        exmple_2 = st.button("What are you looking for ?")
+        exmple_3 = st.button("Your academic projects !")
+
+    
+        st.sidebar.header("Conversation history")
+        
+        clear = st.sidebar.button("Clear Conversation")
+        if (len(st.session_state.conversation)==0) :
+            st.sidebar.write("*** No conversation yet ***")
+            
+
+
+with col2:
+    with st.spinner('Wait for it...'):
+        time.sleep(0.2)
+        fct = fct()
+        words, labels, training, output = fct.getData()
+        model = fct.getModel()
+
+        st.markdown(f'<h3 style="color:#D8B6A4;text-align: center">{"Start talking ..."}</h3>', unsafe_allow_html=True)
+        with st.form(key = "myinp", clear_on_submit = True) :
+            inp = st.text_input("Your question :", placeholder="How are you doing ?")
+            btn = st.form_submit_button("get result")
+        
+        
+        
+        if(clear):
+            inp=''
+            with st.spinner('Wait for it...'):
+                time.sleep(0.2)
+                st.session_state.conversation = []
+                st.sidebar.write("*** No conversation yet ***")
+        
+
+        if (exmple_1):
+            inp = "Who are you ?"
+        elif(exmple_2):
+            inp = "What are you looking for ?"  
+        elif(exmple_3):
+            inp = "Your academic projects"
+ 
+        if inp != "":
+            with st.spinner('Wait for it...'): 
+                time.sleep(0.2)
+                result = fct.getResponse(model, labels, inp, words)
+                st.session_state.conversation.append(inp)
+                st.session_state.conversation.append(result)
+                
+                for i,j in enumerate(st.session_state.conversation[-2:]):
+
+                    if "--- ***** ---" in j:
+                        j = j.split("--- ***** ---")
+                        st.markdown(f'<p style="color:#B85252">{"Khalid (Bot) : "}</p>', unsafe_allow_html=True)
+                        for k in j:
+                            st.write(k)
+                        continue
+
+                    if i%2 == 0:
+                        st.markdown(f'<p style="color:#B85252">{"You : "}</p>', unsafe_allow_html=True)  
+                    else :
+                        st.markdown(f'<p style="color:#B85252">{"Khalid (Bot) : "}</p>', unsafe_allow_html=True)  
+                    st.write(j)
+        sidebar()
+                
+    
---
+++ functions.py
@@ -0,0 +1,57 @@
+import numpy as np
+import tflearn
+import pickle
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import random
+import json
+
+class Functions :
+
+
+    def __init__(self) :
+        with open("intents.json") as file:
+            self.data = json.load(file)
+        with open("data.pickle", "rb") as f:
+            self.words, self.labels, self.training, self.output = pickle.load(f)
+       
+    def getData(self):
+        return self.words, self.labels, self.training, self.output
+
+    def getModel(self):
+
+        net = tflearn.input_data(shape=[None, len(self.training[0])])
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, len(self.output[0]), activation="softmax")
+        net = tflearn.regression(net)
+
+        model = tflearn.DNN(net)
+        model.load("model.tflearn")
+        return model
+
+    def bag_of_words(self, s, words):
+        bag = [0 for _ in range(len(words))]
+        s_words = nltk.word_tokenize(s)
+        s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+        for se in s_words:
+            for i, w in enumerate(words):
+                if w == se:
+                    bag[i] = 1
+                
+        return np.array(bag)
+
+    def getResponse (self, model,labels,inp,words):
+
+        results = model.predict([self.bag_of_words(inp,words)])
+        results_index = np.argmax(results)
+
+        tag = labels[results_index]
+
+        for tg in self.data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        return random.choice(responses)
